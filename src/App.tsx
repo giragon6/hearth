315079f96facc2a0f77afc14e36501bec6bb8b7e
giragon6 <149984595+giragon6@users.noreply.github.com>
2024-10-28 01:27:53 -0400

    Add temporary pixel art

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
+import Stage1Red from './assets/images/dragon-stages/stage-1-red.png';
 import './App.css';
 import { Button } from './components/button/Button';
 
 function App() {
   const [streak, setStreak] = useState(0);
+  const [color, setColor] = useState("red");
+  const [stage, setStage] = useState(1);
   const incrementStreak = () => setStreak(s => s+1);
   return (
     <div className="App">
       Welcome to Hearth!
+      <img src={ Stage1Red }></img>
       Streak: {streak}
       <Button
         onClick={incrementStreak} 
