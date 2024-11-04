33e8df1fd2112acb2a2d2a166d081b9a71db5eaa
giragon6 <149984595+giragon6@users.noreply.github.com>
2024-11-04 00:04:53 -0500

    style: add style for disabled button, make prettier

--- src/components/Streak.tsx
+++ src/components/Streak.tsx
@@ -7,6 +7,13 @@ const Streak = () => {
     const [streak, setStreak] = useStreakStore();
     const [lastDate, setLastDate] = useDateStore();
 
+    const getStage = (streak: number) => {
+        if (streak <= 5) return "0";
+        if (streak <= 15) return "1";
+        if (streak <= 35) return "2";
+        return "3"; 
+    }
+
     const handleIncrement = () => {
         setStreak(prev => (prev + 1));
         const today = new Date();
@@ -17,12 +24,12 @@ const Streak = () => {
 
     return (
         <>
-            <div className='object-center'>Streak: {streak} days</div>
-            <MascotDisplay stage="1" color="red" />
+            <div className='py-4 text-tBase font-serif font-medium object-center'>Streak: {streak} days</div>
+            <MascotDisplay stage={getStage(streak)} color="red" />
             <Button 
                 disabled={lastDate == now.toDateString()} 
                 text="Increase streak" 
-                className='w-full px-2 py-4 bg-primary' 
+                className='w-full px-2 py-4 bg-primary enabled:hover:bg-secondary text-tBase font-serif disabled:opacity-75 disabled:cursor-not-allowed disabled:' 
                 onClick={handleIncrement}
             />
         </>
